2ee7ce23a1a637fa3803251a7bfd93c366c647f5
kmunz@hartenergy.com <kmunz@hartenergy.com>
2023-03-06 17:04:36 -0600

    listener now working with aws-sdk/v3 handleError currently defanged

--- libs/snsClient.js
+++ libs/snsClient.js
@@ -1,4 +1,4 @@
 import { SNSClient } from "@aws-sdk/client-sns";
-const SNS = new SNSClient({ region: 'us-east-1' });
+const snsClient = new SNSClient({ region: 'us-east-1' });
 
 export { snsClient };
